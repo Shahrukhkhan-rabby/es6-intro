41cc52426e976b5dd497338d49754c1937c01c14
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-08-13 20:05:47 +0600

    revision six core parts of JavaScript

---
+++ basic.js
@@ -0,0 +1,48 @@
+// variable
+var deposit = 400;
+// conditon
+if (deposit > 500) {
+    
+}
+else if(deposit < 200){
+
+}
+else if (deposit == 500) {
+    
+}
+if (deposit != 500) {
+    
+}
+if (deposit >= 655) {
+    
+}
+if (deposit <= 500) {
+    
+}
+// array
+const numbers = [45, 6587, 45, 1, 365];
+const numberCount = numbers.length;
+numbers.pop();
+numbers.push(111);
+numbers[2] = 555;
+// check weather 222 included in the array 
+if (numbers.indexOf(222) != -1) {
+    
+}
+if (numbers.includes(222)) {
+    
+}
+
+// loop
+// while, for
+for (const number of numbers) {
+    
+}
+// function
+function fullName (first, second){
+    const name = first + ' ' + second;
+}
+const person = fullName('Rahim', 'Ali');
+
+// Object
+const bottle = {color: 'yellow', contain: 'water', price: 50}
